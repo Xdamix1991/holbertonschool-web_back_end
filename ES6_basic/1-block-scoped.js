88b10fce279eddd257f5a9b54a07973e37fdeeb6
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-11 21:43:59 +0100

    correction

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,5 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    task = false;
-    task2 = true;
-  }
-
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
   return [task, task2];
 }
