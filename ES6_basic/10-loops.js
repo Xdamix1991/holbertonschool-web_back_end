026944aedf9367aa504130f96f8fb925818dd925
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-12 23:31:27 +0100

    task10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let idx = 0;
+  const newArray = [];
+  for (const value of array) {
+    newArray[idx] = appendString + value;
+    idx += 1;
+  }
+
+  return newArray;
+}
