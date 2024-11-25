416f3822dd7155a9299d7b5bcb82a26c653a7821
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-25 11:03:04 +0100

    correctiont

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,8 +2,8 @@ import getListStudentIds from './1-get_list_student_ids';
 
 export default function getStudentIdsSum(students) {
   const listIds = getListStudentIds(students);
-  const initialvalue = 0;
+  const initialValue = 0;
   const result = listIds.reduce((accmulator, currenntValue) => accmulator + currenntValue,
-    initialvalue);
+    initialValue);
   return result;
 }
