5aa6d8d131d33b366b2c3da1c5d2db96bf933540
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-26 15:30:58 +0100

    task3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+  Promise.all([promise1, promise2]).then(([values1, values2]) => {
+    console.log(`${values1.body} ${values2.firstName} ${values2.lastName}`);
+  })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
