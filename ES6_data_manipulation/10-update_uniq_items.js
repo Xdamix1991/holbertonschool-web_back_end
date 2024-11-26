f303b79887b81927d9f8663c38fde6dd5768497f
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-26 11:48:29 +0100

    correction

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(theMap) {
+  if (!(theMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of theMap) {
     if (value === 1) {
       theMap.set(key, 100);
