e9ec136d9a9eb063ed4574d6b4fe3c17ded5fa2d
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-26 18:07:33 +0100

    syntaxe

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,8 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  const promises = [promise1, promise2];
-  return Promise.allSettled(promises)
+  return Promise.allSettled([promise1, promise2])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
