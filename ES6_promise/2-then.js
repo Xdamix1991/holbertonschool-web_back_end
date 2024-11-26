31ee7f9fdbeebd1f72182f63970d0fed9acbd792
Xdamix1991 <bourichalamine1@gmail.com>
2024-11-26 14:51:54 +0100

    hello checker

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,13 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'Success',
-      };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       console.log('Got a response from the API');
-      return new Error();
+      return new Error()
     });
 }
