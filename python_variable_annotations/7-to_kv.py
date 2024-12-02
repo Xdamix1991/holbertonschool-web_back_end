48c61bf9389e2ab8dcb975a798a364853a4c06da
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-02 16:34:48 +0100

    corrected

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,6 +5,4 @@ from typing import List, Union
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
     """ function to return a tuple """
-    sqr: float = v ** 2
-    ressult = (k, sqr)
-    return ressult
+    return (k, v ** 2)
