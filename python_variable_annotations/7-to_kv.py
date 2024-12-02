81f322c73ec5ca8f2d2d698bb4c6883e99ad7bad
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-02 16:31:15 +0100

    task7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" this module contains a function called  to_kv"""
+from typing import List, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[Union[str, float]]:
+    """ function to return a tuple """
+    sqr: float = v ** 2
+    ressult = (k, sqr)
+    return ressult
