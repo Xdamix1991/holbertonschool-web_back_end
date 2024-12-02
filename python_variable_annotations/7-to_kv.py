e5c2405bd67641ba04c5211a6dc4ec324c9577be
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-02 16:35:34 +0100

    corrected

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ this module contains a function called  to_kv"""
-from typing import List, Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ function to return a tuple """
     return (k, v ** 2)
