a94820665273606659a5d4fcd5db724b45b204c8
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-02 15:28:47 +0100

    task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" module containig function floor """
+
+
+def floor(n: float) -> int:
+    """ function floor which takes a float n as argument
+    and returns the floor of the float """
+    return int(n)
