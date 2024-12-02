53bb87839cf8040db9adc53fc2d98526109efddb
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-02 16:58:43 +0100

    task9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" this module contains a function called sum_list """
+from typing import List, Iterable, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence]]:
+    """ function lenght of an element """
+    return [(i, len(i)) for i in lst]
