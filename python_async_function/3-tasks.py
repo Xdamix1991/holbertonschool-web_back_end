d299821a3de3e6efe157f537086d924681fd3798
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-03 10:00:12 +0100

    corrected return type

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,5 +5,5 @@ import time
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     return asyncio.create_task(wait_random(max_delay))
