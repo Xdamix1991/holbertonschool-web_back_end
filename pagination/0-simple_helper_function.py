961c1299b05dcf994f9db44a0db5c6c9a7582267
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-09 13:26:05 +0100

    task0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" this module contains a function index_range """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ function to return page indexs start and end """
+    x = (page - 1) * page_size
+    y = page * page_size
+    return (x, y)
+
+
