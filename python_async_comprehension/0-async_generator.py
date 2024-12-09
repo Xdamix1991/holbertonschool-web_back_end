c8c1070869597c258c318a1916069618827dfd5d
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-09 11:06:15 +0100

    corrected

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,4 +1,4 @@
-#!/usr/bin env python3
+#!/usr/bin/env python3
 """ this module contains a function  """
 import asyncio
 import random
@@ -7,5 +7,6 @@ import random
 async def async_generator():
     """ async function random float return """
     for i in range(10):
-        yield random.uniform(1, 10)
         await asyncio.sleep(1)
+        yield random.uniform(1, 10)
+
