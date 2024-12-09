1748b99c0c8ca0e80fb54e25f9a1920761f2b5f7
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-09 12:20:27 +0100

    task0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" this module contains a function  """
+""" this module contains a function  coroutine"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """ async function random float return """
+    """ async function coroutine random float return """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(1, 10)
+        yield random.uniform(0, 10)
