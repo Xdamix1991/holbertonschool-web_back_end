0ae5d48102e31553e3c81ae3740f5ef4302dd602
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-09 15:13:05 +0100

    task2 corrected

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -40,11 +40,14 @@ class Server:
 
         data_page = self.get_page(page, page_size)
         total_data = len(self.dataset())
+        total_pages = math.ceil(total_data / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
         return {
             'page_size': len(data_page),
             'page': page,
             'data': data_page,
-            'next_page': page + 1,
-            'prev_page': page - 1,
-            'total_page': math.ceil(total_data / page_size)
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_page': total_pages
         }
