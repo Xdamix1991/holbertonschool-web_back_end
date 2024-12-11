d7371ff52996605e3b332871a9fe6cdd167d7a07
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-11 17:05:29 +0100

    task12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" this module contains a function """
+from pymongo import MongoClient
+import pymongo
+
+
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = myclient['logs']
+mycol = mydb['nginx']
+
+logs = mycol.count_documents({})
+print("{} logs".format(logs))
+
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+myquery = {"method": {"$in": method}}
+print("Methods:")
+for x in method:
+    count = mycol.count_documents({"method": x})
+    print("     method {}: {}".format(x, count))
+status = mycol.count_documents({"path": "/status"})
+print("{} status check".format(status))
