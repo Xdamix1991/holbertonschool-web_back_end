e959f31690467a18d4e6bb28654734bba568f7f7
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-12 13:13:28 +0100

    task 1 corrected

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """ function that returns a random float  """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [x async for x in async_generator()]
