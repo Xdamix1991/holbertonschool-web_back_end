6a15b21fcac154a6a63d18ce508369ddc325111c
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-12 12:50:37 +0100

    puting the code in a function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,19 +3,23 @@
 from pymongo import MongoClient
 import pymongo
 
+def x():
+    """ fonction to retrieve data """
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myclient['logs']
+    mycol = mydb['nginx']
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-mydb = myclient['logs']
-mycol = mydb['nginx']
+    logs = mycol.count_documents({})
+    print("{} logs".format(logs))
 
-logs = mycol.count_documents({})
-print("{} logs".format(logs))
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-myquery = {"method": {"$in": method}}
-print("Methods:")
-for x in method:
-    count = mycol.count_documents({"method": x})
-    print("     method {}: {}".format(x, count))
-status = mycol.count_documents( {"method": "GET", "path": "/status"})
-print("{} status check".format(status))
+    print("Methods:")
+    for x in method:
+        count = mycol.count_documents({"method": x})
+        print("     method {}: {}".format(x, count))
+    status = mycol.count_documents( {"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
+
+if "__name__" == "__main__":
+    x()
