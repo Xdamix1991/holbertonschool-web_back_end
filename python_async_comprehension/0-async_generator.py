addedabfcbc572ffe48f6b31cdc096c15bd06061
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-12 13:08:31 +0100

    corrected

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ this module contains a function  coroutine"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ async function coroutine random float return """
     for _ in range(10):
         await asyncio.sleep(1)
