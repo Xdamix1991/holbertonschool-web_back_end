fd7e45deda1c723258bebcbbaca500c58f14d67f
Xdamix1991 <bourichalamine1@gmail.com>
2024-12-12 13:04:29 +0100

    corrected

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """ async function coroutine random float return """
     for i in range(10):
         await asyncio.sleep(1)
