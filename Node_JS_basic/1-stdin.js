4d4432d596586221910e81881281a5181da662be
Xdamix1991 <bourichalamine1@gmail.com>
2025-01-20 13:17:59 +0100

    added module exporting

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name.toString()}`);
-});
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
+function x() {
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('data', (name) => {
+    console.log(`Your name is: ${name.toString()}`);
+  });
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
+}
+module.exports = x;
