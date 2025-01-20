b82484ca5766b625ff3777eb1912ae9336848c95
Xdamix1991 <bourichalamine1@gmail.com>
2025-01-20 13:03:07 +0100

    task1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (name) => {
+  console.log(`Your name is: ${name.toString()}`);
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
