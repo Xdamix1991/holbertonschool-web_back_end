fdb79a497f5ad63b7fafbeab1c159d47f6e35bdc
Xdamix1991 <bourichalamine1@gmail.com>
2025-01-23 15:02:00 +0100

    task4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+// create a server object:
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+app.listen(1245);
