27ed662c0caea0e728fd9971399b4e43866b051d
Xdamix1991 <bourichalamine1@gmail.com>
2025-01-24 13:34:32 +0100

    corrections done

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -5,20 +5,22 @@ const databaseName = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     if (!databaseName) {
-      res.end('No database provided\n');
+      res.end('No database provided');
     } else {
       try {
         const output = await countStudents(databaseName);
         res.end(output);
       } catch (error) {
-        res.end(`${error.message}\n`);
+        res.end(error.message);
       }
     }
   } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
