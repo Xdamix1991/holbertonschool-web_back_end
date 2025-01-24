dc6943344412f6df5382652825ace6365666abed
Xdamix1991 <bourichalamine1@gmail.com>
2025-01-24 14:51:12 +0100

    task7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,26 +1,69 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 
-const app = express();
-const countStudents = require('./3-read_file_async');
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-const database = process.argv[2];
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      lines.shift(); // Remove header row
 
-app.get('/', (req, res) => {
-  res.end('Hello Holberton School!');
-});
-app.get('/students', async (req, res) => {
-  if (!database) {
-    res.send('Cannot load the database');
-    return;
-  } else {
-    try {
-      const output = await countStudents(database);
-      res.send(`This is the list of our students\n${output}`);
-    } catch (error) {
-      res.send(error.message);
+      const students = {};
+      let totalStudents = 0;
+
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+
+        if (firstname && field) {
+          totalStudents += 1;
+
+          if (!students[field]) {
+            students[field] = [];
+          }
+
+          students[field].push(firstname);
+        }
+      });
+
+      let output = `Number of students: ${totalStudents}\n`;
+      for (const [field, names] of Object.entries(students)) {
+        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+      }
+
+      resolve(output.trim());
+    });
+  });
+}
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+
+    const databasePath = process.argv[2];
+    if (!databasePath) {
+      res.end('Cannot load the database');
+      return;
     }
+
+    try {
+      const studentData = await countStudents(databasePath);
+      res.end(studentData);
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
-app.listen(1245);
 
+app.listen(1245);
 module.exports = app;
